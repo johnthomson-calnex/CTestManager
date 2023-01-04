cac6859237885d5c0e7dda15f57093ec3a8e1ffd
John Thomson <john.thomson@calnexsol.com>
2023-01-04 10:23:02 +0000

    basic navbar finished

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,42 @@
+import React from "react"
+import "./NavBar.css"
+
+const NavBar = props => {
+
+    const {activeNavTab,setActiveNavTab} = props
+
+    const navTabClick = tab => {
+        if(activeNavTab)
+            document.getElementById(`nav-${activeNavTab}`).classList.remove("active-nav-tab")
+        document.getElementById(`nav-${tab}`).classList.add("active-nav-tab")
+        setActiveNavTab(tab)
+    }
+
+    
+
+    return(<>
+    
+        <nav className="navbar">
+
+            <ul className="navbar-ul">
+
+                <li className="navbar-li active" onClick={() => navTabClick("tests")} id="nav-tests" >
+                    Tests
+                </li>
+
+                <li className="navbar-li" onClick={() => navTabClick("jobs")} id="nav-jobs">
+                    Jobs
+                </li>
+
+                <li className="navbar-li" onClick={() => navTabClick("reports")} id="nav-reports">
+                    Reports
+                </li>
+
+            </ul>
+
+        </nav>
+    
+    </>)
+}
+
+export default NavBar
