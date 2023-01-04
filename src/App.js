1bbf021e60e3c49337f7d7776529cf950ccb88ca
John Thomson <john.thomson@calnexsol.com>
2023-01-04 15:52:00 +0000

    create context for parameters

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import MainWindow from "./components/main/MainWindow";
 import "./App.css"
+import React, {createContext, useState} from "react"
+
+export const ParameterContext = createContext(null)
 
 function App() {
+
+  const [activeParameter,setActiveParameter] = useState(null)
+
   return (<>
     
-      <HashRouter>
+      <ParameterContext.Provider value={{
+        activeParameter,
+        setActiveParameter
+      }}>
 
-        <Routes>
+        <HashRouter>
 
-          <Route path="/" exact element={<MainWindow />}/>
+          <Routes>
 
-        </Routes>
+            <Route path="/" exact element={<MainWindow />}/>
 
-      </HashRouter>
+          </Routes>
 
+        </HashRouter>
+
+      </ParameterContext.Provider>
 
   </>);
 }
