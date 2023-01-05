ae9c3699f2b20790f352a7b7da33c6a53fae4dc3
John Thomson <john.thomson@calnexsol.com>
2023-01-05 22:46:02 +0000

    run single test function

--- public/functions/TestsFunctions.js
+++ public/functions/TestsFunctions.js
@@ -1,10 +1,18 @@
 const dir = require("node-dir")
 const path = require("path")
+const {spawn} = require("child_process")
+const {Notification} = require("electron")
 const getAllParameters = (event,repositoryRoot) => {
     try {
         const paramFolder = path.join(repositoryRoot,"Parameters")
         dir.promiseFiles(paramFolder)
         .then(paramPaths => {
+            paramPaths = paramPaths.filter(path => {
+                path = path.replace(repositoryRoot+"\\","")
+                const splitPath = path.split("\\")
+                if(!splitPath.includes("__pycache__"))
+                    return path
+            })
             if(paramPaths)
                 event.reply('tests-return-all-parameters', paramFolder, paramPaths)
             
@@ -22,6 +30,7 @@ const getAllParameters = (event,repositoryRoot) => {
 
 const getAllTests = (event,repositoryRoot,ignoreFolders) => {
     try {
+        
         testFolder = repositoryRoot
         dir.promiseFiles(testFolder)
         .then(testPaths => {
@@ -46,4 +55,30 @@ const getAllTests = (event,repositoryRoot,ignoreFolders) => {
     
 }
 
-module.exports = {getAllParameters, getAllTests}
+const runSingleTest = (event,test,parameter, id) => {
+    try {
+        const process = spawn('python', [test, parameter])
+
+        
+
+        process.on('close', code => {
+            console.log(`Child process for ${test} finished with exit code ${code}`)
+            new Notification({title:"Test Finished", body: `${test} has finished`}).show()
+            event.reply("tests-test-finished", id)
+        })
+
+        process.stdout.on('data', data => {
+            console.log(data.toString())
+        })
+
+        process.on("error", err => {
+            console.log(`Error occurred: ${err}` )
+        })
+
+    }
+    catch(e) {
+        console.log(e)
+    }
+}
+
+module.exports = {getAllParameters, getAllTests, runSingleTest}
