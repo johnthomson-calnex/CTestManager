bdb5251701d8cdd7c2bbe629fc0178cc2f8b7592
John Thomson <john.thomson@calnexsol.com>
2023-01-06 14:44:12 +0000

    listen for tests-return-parameter-file

--- src/components/main/MainWindow.js
+++ src/components/main/MainWindow.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react"
-import { ActiveTestsContext, TestContext } from "../../App"
+import { ActiveTestsContext, ParameterContext, TestContext } from "../../App"
 import NavBar from "../navbar/NavBar"
 import TestsWindow from "../tests/TestsWindow"
 const {ipcRenderer} = window.require("electron")
@@ -9,6 +9,7 @@ const MainWindow = props => {
     const [activeNavTab, setActiveNavTab] = useState("tests")
     const {activeTests,setActiveTests} = useContext(ActiveTestsContext)
     const {selectedTest,setSelectedTest} = useContext(TestContext)
+    const {selectedParameter,setSelectedParameter} = useContext(ParameterContext)
 
     ipcRenderer.on('tests-test-finished',(event, id) => {
         const currentActiveTests = [...activeTests]
@@ -23,6 +24,16 @@ const MainWindow = props => {
             const currentSelectedTest = {...selectedTest}
             currentSelectedTest["fileContents"] = contents
             setSelectedTest(currentSelectedTest)
+        }
+        
+    })
+
+    ipcRenderer.on('tests-return-parameter-file', (event,contents) => {
+        if(selectedParameter !== null)
+        {
+            const currentSelectedParameter = {...selectedParameter}
+            currentSelectedParameter["fileContents"] = contents
+            setSelectedParameter(currentSelectedParameter)
         }
         
     })
