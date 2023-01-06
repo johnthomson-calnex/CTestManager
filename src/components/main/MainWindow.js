8dbd99585650f3cb429129ec3d49120d732dfd10
John Thomson <john.thomson@calnexsol.com>
2023-01-06 13:30:21 +0000

    clear active test of recently finished test

--- src/components/main/MainWindow.js
+++ src/components/main/MainWindow.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
+import { ActiveTestsContext } from "../../App"
 import NavBar from "../navbar/NavBar"
 import TestsWindow from "../tests/TestsWindow"
+const {ipcRenderer} = window.require("electron")
 
 const MainWindow = props => {
 
     const [activeNavTab, setActiveNavTab] = useState("tests")
+    const {activeTests,setActiveTests} = useContext(ActiveTestsContext)
+
+    ipcRenderer.on('tests-test-finished',(event, id) => {
+        const currentActiveTests = [...activeTests]
+        const updatedActiveTests = currentActiveTests.filter(test => test.id !== id )
+        setActiveTests(updatedActiveTests)
+
+    })
+    
 
     return (<>
     
